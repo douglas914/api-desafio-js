79ba308f26973b4e4f4cfd3080a50a5a07e06090
Douglas <douglasgalter@gmail.com>
2019-10-16 14:05:02 -0300

    API_desafio_v2

--- README.txt
+++ README.txt
@@ -1,13 +1,10 @@
-Primeira versao da API
-	Capaz de cumprir o requerimento de fornecer o nome e status de acordo com a requisi√ao http.
-	Faltou /nome - RESOLVIDO
-	Porta 5000 - RESOLVIDO
-	Sem formata√ao JSON-RESOLVIDO
-	
-	Solicitacao apenas de porta fornece "null".
-	
-Na versa√o .4 -  implementada a funcao getdadossnmp, obtem todos os dados e responde com o que foi solicitad
+Segunda versao da API
+	Cumpre os requisitos de solicitacoes http para cada porta diferente, em JSON, e api em javascript.
+	A segunda versao inicia a implementacao das funcionalidades de adicionar, deletar e atualizar portas
+		
 
+
+- Nao salva os dados!
 - Sem interatividade
 - Sem docker
 - Sem banco de dados
--- api_desafio.js
+++ api_desafio.js
@@ -11,40 +11,42 @@ var oids_2 = 	[
 			{	
 			nome:"nome1",
 			name_oid:	["1.3.6.1.2.1.1.1.0"],
-			ports:  { port1:["1.3.6.1.2.1.2.2.1.8.1001"],
-				  port2:["1.3.6.1.2.1.2.2.1.8.1002"]
+			ports:  { porta1:["1.3.6.1.2.1.2.2.1.8.1001"],
+				  porta2:["1.3.6.1.2.1.2.2.1.8.1002"]
 				}
 			}
 		]
 
 		
 //console.log(oids_2.lenght)
-for (x in oids_2) {
+//for (x in oids_2) {
 //console.log(x)
 //console.log(oids_2[x].ports);
 //console.log(oids_2[x].name_oid);
 	//for (p in oids_2[x].ports)
 	//	console.log(oids_2[x].ports[p]);
-	var teste = Object.keys(oids_2[x].ports);
-	console.log(teste.length);
-}
+//	var teste = Object.keys(oids_2[x].ports);
+//	console.log(teste.length);
+//}
 
 //console.log(oids_2.sys[0]['name_oid']);
 
 function getdadossnmp(res,solicitacao) {
 
 var resposta_json = 	{
-				acess:"date",
-				sysname:[],
-				estado:[]
+				//acess:"date",
+				//sysname:[],
+				//estado:[]
 			};
-
-
-
+if(!(solicitacao == '/' || solicitacao == '/nome' || /\/porta*/.test(solicitacao))) {
+	res.send("Nao encontrado");
+}else {
 	for (x in oids_2) {
+	var datetime = new Date();
+	resposta_json['acess'] = datetime;
+
+ 	if(solicitacao == '/' || solicitacao == '/nome') {
 	session.get(oids_2[x].name_oid, function(error, varbinds) {
-		var datetime = new Date();
-		resposta_json['acess'] = datetime;
 		if (error) {
 		        console.error (error);
 		    } else {
@@ -54,12 +56,16 @@ var resposta_json = 	{
             			else
 					resposta_json['sysname'] = (varbinds[i].value + "");
 			}
-		
-		//console.log(resposta_json);
+		if (solicitacao == '/nome') {
+		res.json(resposta_json);
+		}	
+	//console.log(resposta_json);
 	});
-
+	}
+	 if(solicitacao == '/') {
 	 for (p in oids_2[x].ports) {
-	var temp = oids_2[x].ports[p];	
+	var temp = oids_2[x].ports[p];
+	console.log(p);	
 	var count = Object.keys(oids_2[x].ports);
 	var z = 0; 
 	session.get (temp, function(error, varbinds) {
@@ -71,122 +77,64 @@ var resposta_json = 	{
                 	console.error (snmp.varbindError (varbinds[i]))
             		else
 			if (varbinds[i].value == 1)
-				resposta_json['estado'][z] = Object.keys(oids_2[x].ports)[z]+"=UP(1)";
+				resposta_json[Object.keys(oids_2[x].ports)[z]] = "=UP(1)";
 
 			else if (varbinds[i].value == 2)
-				resposta_json['estado'][z] = Object.keys(oids_2[x].ports)[z]+"=DOWN(2)";
+				resposta_json[Object.keys(oids_2[x].ports)[z]] = "=DOWN(2)";
 		}
 	z++;
-	//console.log(Object.keys(oids_2[x].ports)[z])
-	//console.log(z)	
-	
 		if ( z == count.length){
-			if (solicitacao == '/'){
 			res.json(resposta_json);
-			} else if (solicitacao == '/porta1') {
-			delete resposta_json.sysname;
-			delete resposta_json.estado[1];
-			res.json(resposta_json);
-			} else if (solicitacao == '/porta2') {
-			delete resposta_json.sysname;
-			delete resposta_json.estado[0];
-			res.json(resposta_json);
-			} else if (solicitacao == '/nome') {
-			delete resposta_json.estado;
-			res.json(resposta_json);
-			} else
-			res.send("Nao encontrado");
-			
+			//return resposta_json;
 		}
         	});
 	}
-    
-         session.trap (snmp.TrapType.LinkDown, function (error) {
+	} else if(!(solicitacao == "/nome")) {
+	//console.log("teste");
+	solicitacao = solicitacao.replace(/\//g,"");
+	//console.log(typeof solicitacao);
+		var temp = oids_2[x].ports[solicitacao];
+		//console.log(temp);
+		//console.log(Object.keys(oids_2[x].ports)[z]);
+		if(!(temp == undefined)) {
+			session.get (temp, function(error, varbinds) {
+		 	if (error) {
+        			console.error (error);
+			 } else {
+		        for (var i = 0; i < varbinds.length; i++)
+        		    if (snmp.isVarbindError (varbinds[i]))
+                		console.error (snmp.varbindError (varbinds[i]))
+	            		else
+				if (varbinds[i].value == 1)
+					resposta_json[solicitacao] = "=UP(1)";
+
+				else if (varbinds[i].value == 2)
+					resposta_json[solicitacao] = "=DOWN(2)";			
+			}
+			res.json(resposta_json);
+			
+		});
+		} else 
+			res.send("Porta nao encontrada");
+
+//	console.log(z);
+//	z++;
+//	console.log(z);
+	}
+ 	}        
+	session.trap (snmp.TrapType.LinkDown, function (error) {
              if (error)
                      console.error (error);
                      });
-
-
-
-
 	}
-
 }
-//getdadossnmp()
+
 
 app.get(/\/*/, function (req, res) {
-//console.log(req.originalUrl);
+	//console.log(req.originalUrl.replace(/\//g,""));
 	getdadossnmp(res,req.originalUrl);
-
-
 });
 
-//app.get('/porta1', function(req,res) {
-//
-//
-//session.get (["1.3.6.1.2.1.2.2.1.8.1001"], function (error, varbinds) {
-//    if (error) {
-//        console.error (error);
-//    } else {
-//       for (var i = 0; i < varbinds.length; i++)
-//            if (snmp.isVarbindError (varbinds[i]))
-//                console.error (snmp.varbindError (varbinds[i]))
-//            else
-//                res.send("Apenas STATUS porta1 = " + varbinds[i].oid + " = " + varbinds[i].value+"\n");
-//    }
-//
-//    // If done, close the session
-//    //     session.close ();
-//         });
-//    
-//         session.trap (snmp.TrapType.LinkDown, function (error) {
-//             if (error)
-//                     console.error (error);
-//                     });
-//});
-//
-//app.get('/porta2', function(req,res) {
-//
-//
-//session.get (["1.3.6.1.2.1.2.2.1.8.1002"], function (error, varbinds) {
-//    if (error) {
-//        console.error (error);
-//    } else {
-//        for (var i = 0; i < varbinds.length; i++)
-//            if (snmp.isVarbindError (varbinds[i]))
-//                console.error (snmp.varbindError (varbinds[i]))
-//            else
-//                res.send("Apenas STATUS porta2 = " + varbinds[i].oid + " = " + varbinds[i].value+"\n");
-//    }
-//
-//    // If done, close the session
-//    //     session.close ();
-//             });
-//             session.trap (snmp.TrapType.LinkDown, function (error) {
-//                 if (error)
-//                     console.error (error);
-//                     });
-//});
-//
-//app.get('/nome', function(req, res) {
-//
-//
-//session.get(["1.3.6.1.2.1.1.1.0"], function (error, varbinds) {
-//  if(error) {
-//	console.error(error);
-//  } else {
-//	for (var i = 0; i < varbinds.length; i++)
-//	if(snmp.isVarbindError (varbinds[i]))
-//		console.error (snmp.varbindError (varbinds[i]))
-//	else
-//	res.send(varbinds[i]);
-//	}
-//});
-//	session.trap (snmp.TrapType.LinkDown, function (error) {
-//	if (error)
-//	console.error(error);
-//	});
-//});
 app.listen(5000, function () {
   console.log('API desafio na porta 5000!\n');
 });
